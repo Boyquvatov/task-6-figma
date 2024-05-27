db915d975c1799a95b31edd1d65bd14355beb395
Your Name <you@example.com>
2024-05-27 18:55:59 +0300

    end

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <div className="link">
+          <a href="#Blok">Block</a>
+          <a href="#">Works</a>
+          <a href="#">Contact</a>
+        </div>
+        <div className="bars">
+          <i className="fa-solid fa-bars"></i>
+        </div>
+      </header>
+      <main id="blok">
+        <div className="main-desc">
+          <h1>Block</h1>
+        </div>
+        <div className="box">
+          <h2>UI Interactions of the week</h2>
+          <div className="time">
+            <h4>12 Feb 2019</h4>
+            <div className="hr"></div>
+            <p>Express, Handlebars</p>
+          </div>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          <div className="hr1"></div>
+        </div>
+        <div className="box">
+          <h2>UI Interactions of the week</h2>
+          <div className="time">
+            <h4>12 Feb 2019</h4>
+            <div className="hr"></div>
+            <p>Express, Handlebars</p>
+          </div>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          <div className="hr1"></div>
+        </div>
+        <div className="box">
+          <h2>UI Interactions of the week</h2>
+          <div className="time">
+            <h4>12 Feb 2019</h4>
+            <div className="hr"></div>
+            <p>Express, Handlebars</p>
+          </div>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          <div className="hr1"></div>
+        </div>
+        <div className="box">
+          <h2>UI Interactions of the week</h2>
+          <div className="time">
+            <h4>12 Feb 2019</h4>
+            <div className="hr"></div>
+            <p>Express, Handlebars</p>
+          </div>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          <div className="hr1"></div>
+        </div>
+      </main>
+      <footer>
+        <div className="icons">
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-linkedin"></i>
+        </div>
+        <p>Copyright ©2020 All rights reserved </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
